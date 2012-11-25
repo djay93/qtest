895b04eae5a2228bc2cfc783fdda2204be1e6187
itsdhana <itsdhana@gmail.com>
2012-11-26 02:37:22 +0530

    Update testcases/test_order_simple_product_norton1600.py

--- testcases/test_order_simple_product_norton1600.py
+++ testcases/test_order_simple_product_norton1600.py
@@ -28,39 +28,3 @@ class TestNorton1600(BaseTestCase):
         self.search_product("Norton 1600")
         #self.wait()
 
-        #verify elements in the left side of product overview
-        main = self.find_element_and_wait(LOC.PRODUCT.MAIN)
-        left = main.find_element(*LOC.PRODUCT.LEFT)
-
-        title = left.find_element(*LOC.PRODUCT.TITLE)
-        product_dict["title"] = title.text
-        self.verify_regexp_matches(title, 'Norton 1600')
-
-        desc = left.find_element(*LOC.PRODUCT.DESCRIPTION)
-        self.verify_eval_true(desc.text >  0 )
-
-        sku = left.find_element(*LOC.PRODUCT.SKU)
-        product_dict["sku"] = sku.text
-        self.verify_eval_true(product_dict["sku"] >  0 )
-
-        #verify kit components
-        left.find_element(*LOC.PRODUCT.KIT_COMPONENTS)
-
-        #verify item summary section on the right
-        right = main.find_element(*LOC.PRODUCT.RIGHT)
-        item = right.find_element(*LOC.PRODUCT.ITEM_SUMMARY)
-        availability = item.find_element(*LOC.PRODUCT.AVAILABILITY)
-        self.verify_regexp_matches(availability, 'In Stock')
-        product_price = item.find_element(*LOC.PRODUCT.PRICE)
-        self.verify_regexp_matches( product_price, "^\$?(\d*\.\d{1,2})$")
-
-        #select item options if available
-        if self.is_element_present(LOC.PRODUCT.OPTION_DIV, item):
-            self.product_select_options()
-
-            options = item.find_elements(*LOC.PRODUCT.OPTION_DIV)
-            product_dict["options"] = [option.find_element_by_css_selector("div span").text for option in options]
-
-        #set qty
-        self.product_set_quantity("10")
-
